20625fa56f23847a99cbd0384829882896680fb0
Jacob Swenson <jaswenson@doterra.com>
2020-05-04 17:30:01 -0600

    Cleaned up logs when connected to multiple servers

--- bot.py
+++ bot.py
@@ -16,9 +16,9 @@ bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
-    print(f'{bot.user} is connected to the following guild:\n')
+    print(f'{bot.user} is connected to the following guild:')
     for guild in bot.guilds:
-        print(f'{guild.name}(id: {guild.id})')
+        print(f'\n{guild.name}(id: {guild.id})')
         members = '\n - '.join([member.name for member in guild.members])
         print(f'Guild Members:\n - {members}')
 
